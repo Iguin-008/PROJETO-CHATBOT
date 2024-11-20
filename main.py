15f842b222d0ac4d1800b6e364b64dbc13168c53
Igor Santos <igoralmeida102080@gmail.com>
2024-11-20 18:09:57 -0300

    Principal

---
+++ main.py
@@ -0,0 +1,161 @@
+
+#NOME DA EQUIPE: Squad Eletronics
+#Igor Almeida
+#Guilherme Gomes 
+#Luan Fernandes
+#Gabriel Borges
+#José Carlos Leite Silva
+#Lucas dos Santos Reis
+#Rafael Dias Farias
+
+import mensagem
+import telebot
+import dotenv
+from os import getenv
+
+dotenv.load_dotenv()
+
+TOKEN_TELEGRAM = getenv("TOKEN_TELEGRAM")
+
+
+#Link do ChatBot: #http://t.me/Lixo_no_Lixo1911_bot
+
+
+bot = telebot.TeleBot(TOKEN_TELEGRAM)
+
+comandos = (
+    "/opcao1 - Dispositivos Pessoais",
+    "/opcao2 - Eletrodomésticos",
+    "/opcao3 - Equipamentos de Escritório",
+    "/opcao4 - Acessórios e Baterias"
+
+)
+
+opcao1_comandos = (
+    "/subopcao1 - Celulares",
+    "/subopcao2 - Tablets",
+    "/subopcao3 - Computadores"
+)
+opcao2_comandos = (
+    "/subopcao1_op2 - Geladeira",
+    "/subopcao2_op2 - Micro-Ondas",
+    "/subopcao3_op2 - Televisores",
+    "/subopcao4_op2 - Aspirador de Pó"
+)
+opcao3_comandos = (
+    "/subopcao1_op3 - Impressora",
+    "/subopcao2_op3 - Mouse",
+    "/subopcao3_op3 - Teclados",
+    "/subopcao4_op3 - Monitores"
+)
+opcao4_comandos = (
+    "/subopcao1_op4 - Cabos",
+    "/subopcao2_op4 - Carregadores",
+    "/subopcao3_op4 - Pilhas e Baterias"
+)
+
+
+@bot.message_handler(commands=["start"])
+def mensagem_boas_vindas(message):
+    bot.send_message(message.chat.id, mensagem.BOAS_VINDAS) 
+    bot.send_message(message.chat.id, comandos[0])
+    bot.send_message(message.chat.id, comandos[1])
+    bot.send_message(message.chat.id, comandos[2])
+    bot.send_message(message.chat.id, comandos[3])
+
+    
+@bot.message_handler(commands=["opcao1"])
+def opcao1(message):
+    bot.send_message(message.chat.id, "Você clicou em Dispositivos Pessoais. Agora escolha uma das subopções:")
+    for subcomando in opcao1_comandos:
+        bot.send_message(message.chat.id, subcomando)
+    
+@bot.message_handler(commands=["subopcao1"])
+def subopcao1(message):
+    bot.send_message(message.chat.id, mensagem.DIS_CELULAR)
+
+@bot.message_handler(commands=["subopcao2"])
+def subopcao2(message):
+    bot.send_message(message.chat.id, mensagem.DIS_TABLET)
+    
+@bot.message_handler(commands=["subopcao3"])
+def subopcao3(message):
+    bot.send_message(message.chat.id, mensagem.DIS_COMPUTADORES)
+    
+
+
+    
+@bot.message_handler(commands=["opcao2"])
+def opcao2(message):
+    bot.send_message(message.chat.id, "Você clicou em Eletrodomésticos. Agora escolha uma das subopções:")
+    for subcomando in opcao2_comandos:
+        bot.send_message(message.chat.id, subcomando)
+
+@bot.message_handler(commands=["subopcao1_op2"])
+def subopcao1(message):
+    bot.send_message(message.chat.id, mensagem.DIS_GELADEIRA)
+
+@bot.message_handler(commands=["subopcao2_op2"])
+def subopcao2(message):
+    bot.send_message(message.chat.id, mensagem.DIS_MICROONDAS)
+    
+@bot.message_handler(commands=["subopcao3_op2"])
+def subopcao3(message):
+    bot.send_message(message.chat.id, mensagem.DIS_TELEVISORES)
+    
+@bot.message_handler(commands=["subopcao4_op2"])
+def subopcao4(message):
+    bot.send_message(message.chat.id, mensagem.DIS_ASPIRADOR)
+        
+        
+        
+
+@bot.message_handler(commands=["opcao3"])
+def opcao3(message):
+    bot.send_message(message.chat.id, "Você clicou em Equipamentos de Escritório. Agora escolha uma das subopções:")
+    for subcomando in opcao3_comandos:
+        bot.send_message(message.chat.id, subcomando)
+        
+@bot.message_handler(commands=["subopcao1_op3"])
+def subopcao1(message):
+    bot.send_message(message.chat.id, mensagem.DIS_IMPRESSORA)
+
+@bot.message_handler(commands=["subopcao2_op3"])
+def subopcao2(message):
+    bot.send_message(message.chat.id, mensagem.DIS_MOUSE)
+    
+@bot.message_handler(commands=["subopcao3_op3"])
+def subopcao3(message):
+    bot.send_message(message.chat.id, mensagem.DIS_TECLADOS)
+    
+@bot.message_handler(commands=["subopcao4_op3"])
+def subopcao4(message):
+    bot.send_message(message.chat.id, mensagem.DIS_MONITORES)    
+    
+
+    
+        
+@bot.message_handler(commands=["opcao4"])
+def opcao4(message):
+    bot.send_message(message.chat.id, "Você clicou em Acessórios e Baterias. Agora escolha uma das subopções:")
+    for subcomando in opcao4_comandos:
+        bot.send_message(message.chat.id, subcomando)
+        
+@bot.message_handler(commands=["subopcao1_op4"])
+def subopcao1(message):
+    bot.send_message(message.chat.id, mensagem.DIS_CABOS)
+
+@bot.message_handler(commands=["subopcao2_op4"])
+def subopcao2(message):
+    bot.send_message(message.chat.id, mensagem.DIS_CARREGADORES)
+    
+@bot.message_handler(commands=["subopcao3_op4"])
+def subopcao3(message):
+    bot.send_message(message.chat.id, mensagem.DIS_PILHAS_BATERIAS)
+    
+        
+        
+if __name__ == "__main__":
+    bot.polling()
+    
+    
---
+++ mensagem.py
@@ -0,0 +1,270 @@
+
+#Mensagens boas vindas
+
+BOAS_VINDAS = """
+👋🏾 Olá, seja bem-vindo ao Lixo no Lixo!
+
+Você sabia que o descarte inadequado de lixo eletrônico pode causar sérios danos à saúde humana e ao meio ambiente? 
+
+Dispositivos eletrônicos contêm substâncias químicas perigosas, como chumbo, cádmio, mercúrio e berílio. 
+O descarte incorreto desses materiais libera metais pesados no meio ambiente, contaminando o solo e a água.
+
+Por isso, estou aqui para ajudar você a descobrir onde descartar seu lixo eletrônico de forma responsável, sem prejudicar o meio ambiente.
+
+Aqui estão algumas opções para começar:
+
+"""
+
+
+#Mensagem Dispositivos Pessoais
+
+DIS_CELULAR = """
+Em Brasília, você pode descartar celulares em pontos de coleta específicos para eletrônicos, como:
+
+• Lojas de tecnologia e operadoras de telefonia (ex.: Samsung, Vivo, Claro, TIM, etc.).
+• ONGs que trabalham com reciclagem de eletrônicos, como a Programando o Futuro, que atua na região.
+• Centros de reciclagem especializados, como o SLU (Serviço de Limpeza Urbana).
+• Ecopontos, administrados pelas administrações regionais, no lugar das prefeituras.
+
+Para mais informações, você pode consultar o site do Instituto de Defesa do Consumidor (Idec) ou entrar em contato com a Administração Regional da sua área.
+
+Curiosidade: Antes de descartar, considere reutilizar seu celular como câmera de segurança, monitor para crianças ou dispositivo de controle remoto inteligente.
+Nunca descarte o aparelho celular inservível no lixo comum. O descarte correto de eletrônicos contribui para a economia circular e reduz o impacto ambiental.
+
+Além disso, você pode reaproveitar seu smartphone antigo para outras utilidades, como: relógio despertador, controle remoto, sistema de vigilância, livro de receitas, gravador de voz, scanner de documentos e babá eletrônica.
+
+Para recomeçar, digite "/start".
+"""
+
+DIS_TABLET = """
+Para descartar um tablet, você pode:
+
+• Levá-lo a um ecoponto ou posto de coleta especializado.
+• Entregá-lo em um ponto de coleta de uma operadora, como Vivo ou Claro.
+• Entregá-lo em um Ponto de Entrega Voluntária (PEV), que geralmente ficam em parques, shoppings, supermercados e centros comerciais.
+
+Antes de descartar o tablet, é importante:
+
+• Desconectar o aparelho.
+• Apagar todos os dados pessoais.
+• Fazer backup de todos os dados importantes.
+• Resetar o aparelho para as configurações de fábrica.
+
+A reciclagem de eletrônicos é realizada da seguinte forma:
+
+• As empresas coletoras triagem e separam os equipamentos que ainda podem ser utilizados dos que não têm mais serventia.
+• As carcaças dos objetos sem utilidade são trituradas e separadas de acordo com o material.
+• Os materiais valiosos são enviados para empresas que os reutilizam na fabricação de novos produtos.
+
+Para recomeçar, digite "/start".
+
+"""
+
+DIS_COMPUTADORES = """
+Para descartar um computador, você pode:
+
+• Levá-lo a um ponto de coleta especializado, como um ecoponto, centro de reciclagem, cooperativa de catadores ou posto de recolhimento.
+ Os ecopontos recebem eletrônicos e outros materiais recicláveis. 
+ 
+    Alguns estão localizados em:
+    
+Asa Sul: Quadra 710/711.
+Ceilândia: QNN 14.
+Taguatinga: QNL 2.
+
+• Devolver o computador ao fabricante, que é responsável pela destinação correta.
+• Doar ou vender o computador, se ele ainda estiver em bom estado.
+
+Antes de descartar o computador, é importante:
+
+• Apagar todos os dados pessoais.
+• Desconectar o computador.
+• Manter o computador limpo.
+
+A reciclagem de computadores é importante para evitar contaminações e reaproveitar os materiais. A carcaça do computador é triturada e separada por tipo de material, que pode ser vendido para outras empresas ou incinerado para gerar energia.
+
+Para recomeçar, digite "/start".
+
+"""
+
+
+#Mensagem Eletrodomésticos
+
+DIS_GELADEIRA = """
+Para descartar uma geladeira de forma correta:
+
+• Antes de tudo, desligue a geladeira e esvazie completamente seu interior. Certifique-se de que não há alimentos ou líquidos.
+• Se o eletrodoméstico estiver funcionando, você pode doar para uma família necessitada.
+• Existem empresas que auxiliam no descarte consciente e também aceitam o produto na troca por um novo adquirido na loja, como a Samsung. Contato: +55 11 4004-0000.
+• Você pode verificar se a geladeira ainda é útil, pois existem plataformas de vendas onde é possível vender ou trocar o eletrodoméstico.
+
+Para recomeçar, digite "/start".
+
+"""
+
+DIS_MICROONDAS = """
+Micro-ondas possuem componentes que são perigosos para o meio ambiente, como, por exemplo, capacitores. Para fazer o descarte correto, você deve:
+
+• Verificar se há ecopontos em sua cidade que aceitam micro-ondas.
+• Conferir se o micro-ondas ainda é útil. Existem plataformas de vendas onde você pode vender ou trocar o eletrodoméstico.
+• Procurar empresas que auxiliam no descarte consciente e também aceitam o produto na troca por um novo adquirido na loja, como a Samsung. Contato: +55 11 4004-0000.
+
+Para recomeçar, digite "/start".
+
+"""
+
+DIS_TELEVISORES = """
+As telas de LCD, por exemplo, possuem arsênio no vidro e mercúrio nas lâmpadas fluorescentes, que são usadas para iluminar a tela. Como se sabe, tanto o mercúrio quanto o arsênio possuem potencial tóxico. Por isso, para descartar uma televisão de maneira correta, você pode:
+
+• Contatar o fabricante, pois a maioria das empresas oferece serviços de logística reversa.
+• Doar: se a televisão ainda funciona ou está em bom estado para conserto, doe para instituições de caridade, escolas ou pessoas necessitadas.
+• Vender: plataformas de itens novos e usados, como OLX, são uma boa opção para dar uma nova vida ao aparelho.
+• Empresas que auxiliam no descarte consciente também aceitam o produto na troca por um novo adquirido na loja, como a Samsung. Contato: +55 11 4004-0000.
+Pontos de coleta em Brasília:
+• Águas Claras: Metrô – Estação Águas Claras, Avenida Pau Brasil, 2500.
+• Parque Ecológico: Av. Parque Águas Claras – Águas Claras-DF.
+• Unieuro: Avenida Castanheira, Lote 3700, Águas Claras – DF.
+• Areal: Parque Ecológico do Areal, SHA Chácara 131, Arniqueiras, Brasília-DF.
+
+Para recomeçar, digite "/start".
+
+"""
+
+DIS_ASPIRADOR = """
+Para descartar um aspirador de pó de forma correta, você pode:
+
+• Verificar ecopontos, centros de reciclagem ou locais especializados em lixo eletrônico na sua cidade.
+• Muitas empresas oferecem programas de logística reversa, com serviços de coleta de eletrodomésticos antigos.
+• Participar de campanhas de coleta: várias cidades promovem eventos ou campanhas para o descarte de eletrônicos.
+
+Locais de coleta em Brasília:
+
+Asa Sul:
+
+• Estação de Reciclagem Pão de Açúcar - 403 Sul. Endereço: Superquadra Sul 403, Asa Sul.
+• Administração Regional do Plano Piloto. Endereço: Setor de Áreas Municipais, Quadra 514/515, Bloco A, Asa Sul.
+
+Taguatinga:
+
+• Ecoponto do SLU – Taguatinga. Endereço: QNL 2, Área Especial, Taguatinga Norte.
+
+Programas Gerais:
+
+• Parques Ecológicos. Pontos de coleta em parques, como o Parque da Cidade.
+
+Para recomeçar, digite "/start".
+
+"""
+
+
+#Mensagem Equipamentos de Escritório
+
+DIS_IMPRESSORA = """
+Impressoras contêm metais pesados que são prejudiciais à saúde humana e ao meio ambiente. Por isso, para realizar o descarte de uma impressora de forma correta, você deve:
+
+• Retirar os cartuchos de tinta, pois eles contêm resíduos químicos. Algumas fabricantes de tinta aceitam cartuchos usados para reciclagem.
+• Componentes eletrônicos, como circuitos, motores e cabos, contêm metais como cobre e alumínio, que podem ser reciclados.
+• Procurar recicladoras especializadas: empresas especializadas em reciclagem de lixo eletrônico podem aceitar impressoras para reaproveitamento.
+• Participar do Programa Reciclotech, que coleta equipamentos como computadores, celulares, tablets e outros, promovendo o recondicionamento, doação para instituições como escolas e hospitais, além de oferecer cursos de capacitação.
+
+Locais de coleta em Brasília:
+
+• Águas Claras: Metrô – Estação Águas Claras, Avenida Pau Brasil 2500.
+• Parque Ecológico: Av. Parque Águas Claras – Águas Claras-DF.
+• Unieuro: Avenida Castanheira, Lote 3700, Águas Claras – DF.
+• Areal: Parque Ecológico do Areal, SHA Chácara 131, Arniqueiras, Brasília-DF.
+
+Para recomeçar, digite "/start".
+
+"""
+
+DIS_MOUSE = """
+Para descartar um mouse com segurança você deve: 
+
+•	Não jogar o mouse em lixo comum, pois isso causará contaminação
+•	Confira se ainda é utilizável, ou pode ser consertado, podendo ser doado para uma loja que aceite itens usados
+•	Localizar ecopontos ou empresas que descartam lixos eletrônicos, como por exemplo shoppings, supermercados ou lojas de eletrônicos podem ter caixas de coleta de lixo eletrônico 
+•	Estação de Reciclagem Pão de Açúcar - Localizada na Asa Sul (403 Sul), essa estação aceita pequenos eletrônicos, pilhas e baterias.
+
+Para recomeçar, digite "/start".
+
+"""
+
+DIS_TECLADOS = """
+O descarte de um teclado de computador é um pouco mais complicado, pois a maior parte do material plástico que compõe o teclado não é reciclável. No entanto, as placas metálicas e os circuitos podem ser reutilizados, assim como o teclado inteiro. Para um descarte correto, você pode:
+
+• Verificar locais que aceitam doações de teclados antigos para reutilização em projetos sociais.
+• Procurar cooperativas que recebem certos materiais contidos no objeto.
+• Evitar o descarte incorreto e não jogar o teclado no lixo comum, pois isso impede a reciclagem e pode causar contaminação ambiental.
+• Estação de Reciclagem Pão de Açúcar: Localizada na Asa Sul (403 Sul), essa estação aceita pequenos eletrônicos, pilhas e baterias.
+
+Para recomeçar, digite "/start".
+
+"""
+
+DIS_MONITORES = """
+Para fazer o descarte correto de um monitor, você deve:
+
+• O descarte de um monitor usado deve ser feito de maneira sustentável, pois muitas vezes esse equipamento pode ser reutilizado. Caso esteja funcionando, o monitor pode ser doado para ONGs, escolas ou pessoas que precisem.
+• Procurar ecopontos ou empresas de reciclagem na sua região.
+• Várias lojas de eletrônicos, supermercados e cooperativas têm programas de descarte eletrônico.
+• Existem também programas de logística reversa, onde muitas marcas oferecem esse serviço com seus produtos, como por exemplo: Samsung, Dell, HP e LG.
+• Monitores contêm vários componentes tóxicos, como chumbo e mercúrio, que podem contaminar o solo e a água se descartados de forma inadequada.
+
+Para recomeçar, digite "/start".
+
+"""
+
+
+#Mensagem Acessórios e baterias
+
+DIS_CABOS = """
+Para descartar cabos, pode-se:
+
+• Levá-los a pontos de coleta especializados, como ecopontos, centros de reciclagem ou programas de coleta seletiva organizados pelas prefeituras.
+• Contratar empresas especializadas em reciclagem, que podem oferecer serviços de coleta em casa.
+Se os cabos estiverem em bom estado, é possível reutilizá-los ou doá-los para alguém que esteja fazendo uma obra ou reforma.
+
+Os cabos são compostos por cobre e plástico, materiais recicláveis, que podem ser reaproveitados em outros produtos ou tratados para gerar energia. A reciclagem é importante para evitar o acúmulo de lixo eletrônico.
+
+Para recomeçar, digite "/start".
+
+"""
+
+DIS_CARREGADORES = """
+Para descartar carregadores, pode:
+
+•	Levá-los a um posto de coleta de reciclagem de celulares, geralmente localizado em lojas de operadoras de telefonia móvel 
+•	Entrar em contato com o fabricante do aparelho para enviar os resíduos 
+•	Levá-los a um ecoponto ou posto de coleta especializado 
+•	Procurar por cooperativas que recebem esse tipo de material 
+•	Usar o serviço de coleta em casa de uma empresa especializada 
+
+Algumas operadoras, como a Claro e a Vivo, têm programas de reciclagem de lixo eletrônico. A Claro tem urnas coletoras em suas lojas e agentes autorizados. A Vivo tem lixeiras identificadas em suas lojas e revendas para descartar resíduos eletrônicos de pequeno porte. 
+Para minimizar o impacto ambiental, é importante adotar práticas adequadas de descarte de lixo eletrônico, como a reutilização de dispositivos funcionais.
+
+Para recomeçar, digite "/start".
+
+"""
+
+DIS_PILHAS_BATERIAS = """
+Pilhas e baterias devem ser descartadas em locais específicos e adequados, pois contêm substâncias tóxicas que podem contaminar o meio ambiente e causar problemas de saúde. 
+
+Para descartar pilhas e baterias corretamente, você pode:
+
+•	Separar os resíduos do restante do lixo 
+•	Armazenar em um recipiente seco, bem ventilado e que não receba luz solar 
+•	Levar os resíduos a um ponto de coleta, como:
+    o	Lojas que vendem os produtos 
+    o	Redes de coleta em empresas e subprefeituras 
+    o	Caixas coletoras em supermercados, farmácias, bancos e outros estabelecimentos comerciais 
+    o	Ecopontos em parques e praças, disponibilizados pelas secretarias de Meio Ambiente em cidades grandes 
+    o	Assistências técnicas autorizadas 
+    
+Pilhas comuns, como as recarregáveis, devem ser devolvidas ao fabricante. 
+O descarte incorreto de pilhas e baterias pode contaminar o solo, os lençóis freáticos e causar graves problemas de saúde em animais e seres humanos. 
+
+Para recomeçar, digite "/start".
+
+"""
+
